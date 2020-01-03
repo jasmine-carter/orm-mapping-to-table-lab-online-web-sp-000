d3699c9eeab71cd71edebbe374c5fd9b1a8e7633
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-03 02:04:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
     INSERT INTO students (name, grade)
     VALUES (?, ?)
     SQL
-    DB[:conn].excecute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowi() FROM students")[0][0]
   end
 
